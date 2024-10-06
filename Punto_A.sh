7b242fcd97c09cc1f151cadd1487190713da7df6
deltron9 <cristian_collante@hotmail.com>
2024-10-06 22:00:11 +0000

    ADD: añadido script para realizar creación de directorios

---
+++ Punto_A.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Punto A: Estructura simétrica"
+
+sudo mkdir -p /Examenes-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
+
+
+tree /Examenes-UTN/
+
+
+echo "Script ejecutado excitantemente!!!"
---
+++ RTA_SCRIPT_Examen_20241006/Punto_A.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Punto A: estructura de directorio simetrica"
+
+sudo mkdir -p /Examenes-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
+
+
+tree /Examenes-UTN/
+
+
+echo "Script ejecutado exitantemente!!"
