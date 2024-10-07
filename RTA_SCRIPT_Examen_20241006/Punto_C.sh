d25154edbab7f2b497e4f7f747c2918a7eee08bf
deltron9 <cristian_collante@hotmail.com>
2024-10-07 20:27:04 +0000

    fix: reparación de error en archivo de script

--- RTA_SCRIPT_Examen_20241006/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_C.sh
@@ -1,41 +1,49 @@
 #!/bin/bash
 
-echo "creo los usuarios"
+echo "Crando los usuarios..."
+echo
 sudo useradd p1c2_2024_A1
 sudo useradd p1c2_2024_A2
 sudo useradd p1c2_2024_A3
 sudo useradd p1c2_2024_P1
 
-echo "creamos los grupos"
+echo "Creando de los grupos..."
+echo
 sudo groupadd p1c2_2024_g_Alumno
 sudo groupadd p1c2_2024_g_Profesores
 
-echo "modificamos los grupos de los usuarios"
+echo "Modificación de los grupos de los usuarios"
+echo
 sudo usermod -aG p1c2_2024_gAlumno p1c2_2024_A1
 sudo usermod -aG p1c2_2024_gAlumno p1c2_2024_A2
 sudo usermod -aG p1c2_2024_gAlumno p1c2_2024_A3
 sudo usermod -aG p1c2_2024_gProfesores p1c2_2024_P1
 
 
-echo "asignamos claves (misma clave que el usuario)"
+echo "Asignando la misma clave que el usuario"
+echo
 echo "p1c2_2024_A1:p1c2_2024_A1" | sudo chpasswd
 echo "p1c2_2024_A2:p1c2_2024_A2" | sudo chpasswd
 echo "p1c2_2024_A3:p1c2_2024_A3" | sudo chpasswd
 echo "p1c2_2024_P1:p1c2_2024_P1" | sudo chpasswd
 
-echo "ajustamos los permisos"
+echo "Ajustando los permisos..."
+echo
 sudo chmod -R 750 /Examenes-UTN/alumno_1
 sudo chmod -R 760 /Examenes-UTN/alumno_2
-sudo chmos -R 700 /Examenes-UTN/alumno_3
+sudo chmod -R 700 /Examenes-UTN/alumno_3
 
-echo "ajusto los dueños"
+
+echo "Ajustando los dueños..."
+echo
 sudo chown -R p1c2_2024_A1:p1c2_2024_A1 /Examenes-UTN/alumno_1  
 sudo chown -R p1c2_2024_A2:p1c2_2024_A2 /Examenes-UTN/alumno_2
 sudo chown -R p1c2_2024_A3:p1c2_2024_A3 /Examenes-UTN/alumno_3
 sudo chown -R p1c2_2024_P1:p1c2_2024_g_Profesores /Examenes-UTN/profesores
 
 
-echo "Valido los permisos y owners"
+echo "Validando los permisos y owners..."
+echo
 
 sudo ls -ld /Examenes-UTN/alumno_1  
 sudo ls -l /Examenes-UTN/alumno_1 
@@ -50,22 +58,23 @@ sudo ls -ld /Examenes-UTN/profesores
 sudo ls -l /Examenes-UTN/profesores  
 echo
 
+echo "Creando archivo con p1c1_2024_u1 "
 echo
-echo "Creo archivo con p1c1_2024_u1 "
 sudo  whoami > Examenes-UTN/alumno_1/validar1.txt p1c2_2024_A1 
 sudo  whoami > Examenes-UTN/alumno_2/validar2.txt p1c2_2024_A2
 sudo  whoami > Examenes-UTN/alumno_3/validar3.txt p1c2_2024_A3
 sudo  whoami > /Examenes-UTN/profesores/validar4.txt p1c2_2024_P1
 
+echo "Permisos de los archivos creados: "
 echo
-echo "Mostramos permisos de los archivos creados"
 sudo ls -l Examenes-UTN/alumno_1/validar1.txt
 sudo ls -l Examenes-UTN/alumno_2/validar2.txt
 sudo ls -l Examenes-UTN/alumno_3/validar3.txt
 sudo ls -l Examenes-UTN/profesores/validar4.txt
 
-echo
+
 echo "Mostramos los archivos: "
+echo
 sudo cat Examenes-UTN/alumno_1/validar1.txt
 sudo cat Examenes-UTN/alumno_2/validar2.txt
 sudo cat Examenes-UTN/alumno_3/validar3.txt
