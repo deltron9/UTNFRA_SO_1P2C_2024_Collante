dd11011e700a284d3b3d4fc3a1e90093419bd001
deltron9 <cristian_collante@hotmail.com>
2024-10-07 17:26:44 +0000

    feat: añadir script para creación de directorios simetricos

--- RTA_SCRIPT_Examen_20241006/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_A.sh
@@ -5,6 +5,6 @@ echo "Punto A: estructura de directorio simetrica"
 sudo mkdir -p /Examenes-UTN/{alumno_{1..3}/parcial_{1..3},{profesores}}
 
 echo
-echo "Script ejecutado exitantemente!!"
+echo "Script ejecutado exitantemente!"
 tree /Examenes-UTN
 
